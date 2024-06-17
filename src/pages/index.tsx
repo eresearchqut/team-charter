891d94d9bea0abbbe6c5674a79c76422bc5889f9
ryan-bennett <ryan.bennett.au@gmail.com>
2024-06-18 08:05:44 +1000

    Linting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import {
-  Heading,
-  Box,
-  Center,
-  UnorderedList,
-  ListItem,
-  Flex,
-} from "@chakra-ui/react";
-
-type Routes = {
-  text: string;
-  route: string;
-};
+import { Heading, Box, Center } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
-  const routesArr: Routes[] = [
-    { text: "See Counter example component", route: "counter-example" },
-    { text: "See Fetch example component", route: "fetch-example" },
-  ];
-
   return (
     <div>
       <Head>
         <title>eResearch Team Charter</title>
-        <meta
-          name="description"
-          content="Our Team Charter"
-        />
+        <meta name="description" content="Our Team Charter" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Box>
