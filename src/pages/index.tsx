8c85f53a97367ba96e10f53ef9ec20f99ceda749
ryan-bennett <ryan.bennett.au@gmail.com>
2024-06-19 10:33:58 +1000

    feature: team charter boards add to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Heading, Box, Center } from "@chakra-ui/react";
+import teamCharter from "../../public/team-charter.json";
+import { StickyNotesBoardCarousel } from "../components";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>eResearch Team Charter</title>
         <meta name="description" content="Our Team Charter" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box>
-        <Center>
-          <Heading
-            role="heading"
-            as="h1"
-            color="white"
-            borderRadius={4}
-            mt={8}
-            p={4}
-            bg="#2464ec"
-          >
-            eResearch Team Charter
-          </Heading>
-        </Center>
-      </Box>
-    </div>
+      <StickyNotesBoardCarousel {...teamCharter} />
+    </>
   );
 };
 
